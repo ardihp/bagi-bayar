6f21147127b2237830dd34173a58ca23c944b6fd
ardihp <ardihp8@gmail.com>
2026-06-08 22:39:58 +0700

    fix(layout): navbar backdrop filter

--- components/layout/landing-header.tsx
+++ components/layout/landing-header.tsx
@@ -3,7 +3,11 @@
 import { useState } from "react";
 import Link from "next/link";
 import { HugeiconsIcon } from "@hugeicons/react";
-import { SplitIcon, Menu01Icon, Cancel01Icon } from "@hugeicons/core-free-icons";
+import {
+  SplitIcon,
+  Menu01Icon,
+  Cancel01Icon,
+} from "@hugeicons/core-free-icons";
 import { SectionMark } from "@/modules/landing/view";
 
 const navbarItems = [
@@ -22,12 +26,21 @@ export default function HeaderLanding() {
     setMobileMenuOpen(false);
   };
 
+  const handleBackToTopClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    setMobileMenuOpen(false);
+  };
+
   return (
-    <nav className="sticky top-0 z-40 bg-background/80 backdrop-blur-md md:bg-background md:backdrop-blur-none md:relative md:z-auto grid grid-cols-1 lg:grid-cols-[1fr_minmax(0,1536px)_1fr] border-b-2 border-dashed border-secondary/10">
+    <nav className="sticky top-0 z-40 bg-background/80  md:bg-background md:relative md:z-auto grid grid-cols-1 lg:grid-cols-[1fr_minmax(0,1536px)_1fr] border-b-2 border-dashed border-secondary/10">
       <SectionMark section="Header" />
-      <div className="container flex items-center border-x-2 border-secondary/10 border-dashed py-4 md:py-6 px-4 md:px-8 lg:px-12">
+      <div className="container flex items-center border-x-2 border-secondary/10 backdrop-blur-md md:backdrop-blur-none border-dashed py-4 md:py-6 px-4 md:px-8 lg:px-12">
         <div className="w-auto md:w-50 pb-2">
-          <Link href="/" className="flex items-center gap-2 md:gap-3 group">
+          <Link
+            href="/"
+            className="flex items-center gap-2 md:gap-3 group"
+            onClick={handleBackToTopClick}
+          >
             <div className="relative top-0 p-1.5 rounded-lg bg-linear-to-bl from-primary to-secondary shadow-[0_5px_0] shadow-secondary/30 group-active:top-1.25 group-active:shadow-none duration-200">
               <HugeiconsIcon
                 icon={SplitIcon}
@@ -77,7 +90,7 @@ export default function HeaderLanding() {
 
       {/* Mobile menu overlay */}
       {mobileMenuOpen && (
-        <div className="md:hidden absolute top-full left-0 right-0 z-50 border-2 border-dashed border-secondary/10 bg-background/60 backdrop-blur-lg shadow-lg">
+        <div className="md:hidden absolute top-full left-0 right-0 z-50 border-2 border-dashed border-secondary/10 bg-background/60 backdrop-blur-md md:backdrop-blur-none shadow-lg">
           <div className="flex flex-col px-4 py-4 gap-4">
             {navbarItems.map((item) => (
               <div

b7a2cf6adca4b6dda801f7e44845d847b532e79e
ardihp <ardihp8@gmail.com>
2026-07-08 23:17:11 +0700

    fix(components): invalid import on floating button

--- components/ui/floating-button.tsx
+++ components/ui/floating-button.tsx
@@ -1,9 +1,10 @@
-import { IconPlus } from "@tabler/icons-react";
+import { Plus } from "@hugeicons/core-free-icons";
+import { HugeiconsIcon } from "@hugeicons/react";
 
 export default function FloatingButton() {
   return (
     <div className="fixed bottom-4 right-4 p-4 border rounded-full">
-      <IconPlus />
+      <HugeiconsIcon icon={Plus} />
     </div>
   );
 }

7ccb234abaaad327fb226d21ddf36eb095aa98a9
Ardiansyah Halim Putra <90186564@hq.bri.co.id>
2026-08-19 16:26:11 +0700

    fix(hooks): function sign in with otp

--- hooks/use-auth.ts
+++ hooks/use-auth.ts
@@ -3,37 +3,10 @@
 import { createServerSupabase } from "@/lib/supabase/server";
 import { SignInWithEmail, SignUpWithEmail } from "@/types/auth";
 
-export const signInWithProvider = async () => {
-  const client = await createServerSupabase();
-
-  const { data, error } = await client.auth.signInWithOAuth({
-    provider: "google",
-    options: {
-      redirectTo: "/auth/callback",
-    },
-  });
-
-  console.log(data, error);
-
-  if (error) {
-    return {
-      success: false,
-      message: error?.message,
-    };
-  }
-
-  return {
-    success: true,
-    data: data,
-    message: "Success login",
-  };
-};
-
 export const signInWithEmail = async (params: SignInWithEmail) => {
   const client = await createServerSupabase();
-  const { data, error } = await client.auth.signInWithPassword({
+  const { data, error } = await client.auth.signInWithOtp({
     email: params.email,
-    password: params.password,
   });
 
   if (error) {
@@ -75,4 +48,24 @@ export const signUpWithEmail = async (params: SignUpWithEmail) => {
 export const signOutUser = async () => {
   const client = await createServerSupabase();
   await client.auth.signOut();
+};
+
+export const signInWithOTP = async (params: { email: string }) => {
+  const client = await createServerSupabase();
+  const { data, error } = await client.auth.signInWithOtp({
+    email: params.email,
+  });
+
+  if (error) {
+    return {
+      success: false,
+      message: error?.message,
+    };
+  }
+
+  return {
+    success: true,
+    user: data.user,
+    message: "OTP Code sent, please kindly check your email.",
+  };
 };

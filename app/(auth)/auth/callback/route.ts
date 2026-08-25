015f1c26e5fdb2e86af3c88910b204bfab0dce24
ardihp <ardihp8@gmail.com>
2026-08-25 23:21:03 +0700

    fix(debug): callback issue

--- app/(auth)/auth/callback/route.ts
+++ app/(auth)/auth/callback/route.ts
@@ -18,17 +18,7 @@ export async function GET(request: Request) {
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (error) return closePopupAndNotify("login-failed");
-
-    const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-    const isLocalEnv = process.env.NODE_ENV === "development";
-    if (isLocalEnv) {
-      // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-      return closePopupAndNotify("login-success");
-    } else if (forwardedHost) {
-      return NextResponse.redirect(`https://${forwardedHost}${next}`);
-    } else {
-      return closePopupAndNotify("login-success");
-    }
+    return closePopupAndNotify("login-success");
   } else {
     return closePopupAndNotify("login-failed");
   }

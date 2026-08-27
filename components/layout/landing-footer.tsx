c3fd3f2185963dc337dc8ac25e6470b307874151
Ardiansyah Halim Putra <90186564@hq.bri.co.id>
2026-08-27 16:19:26 +0700

    fix(landing): assign link to components

--- components/layout/landing-footer.tsx
+++ components/layout/landing-footer.tsx
@@ -1,14 +1,24 @@
+"use client";
+
 import { SplitIcon } from "@hugeicons/core-free-icons";
 import { HugeiconsIcon } from "@hugeicons/react";
 import Link from "next/link";
 
 export default function FooterLanding() {
+  const handleBackToTopClick = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <footer className="grid grid-cols-1 lg:grid-cols-[1fr_minmax(0,1536px)_1fr] border-t-2 border-secondary/10 border-dashed">
       <div className="hidden lg:flex items-end justify-end p-3 min-w-20 w-full" />
-      <div className="container flex flex-col md:flex-row justify-between items-center gap-4 border-x-0 lg:border-x-2 border-secondary/10 border-dashed pt-6 md:pt-8 pb-8 md:pb-10 px-4 md:px-8 lg:px-12">
+      <div className="container flex flex-row justify-between items-center gap-4 border-x-0 lg:border-x-2 border-secondary/10 border-dashed pt-6 md:pt-8 pb-8 md:pb-10 px-4 md:px-8 lg:px-12">
         <div className="w-auto md:w-50">
-          <Link href="/" className="flex items-center gap-2 md:gap-3 group">
+          <Link
+            href="/"
+            className="flex items-center gap-2 md:gap-3 group"
+            onClick={handleBackToTopClick}
+          >
             <div className="relative top-0 p-1.5 rounded-lg bg-linear-to-bl from-primary to-secondary shadow-[0_5px_0] shadow-secondary/30 group-active:top-1.25 group-active:shadow-none duration-200">
               <HugeiconsIcon
                 icon={SplitIcon}
@@ -21,8 +31,7 @@ export default function FooterLanding() {
         </div>
 
         <p className="text-xs text-primary text-center md:text-right">
-          © {new Date().getFullYear()} Expenses Splitter. Clarity in motion for
-          the modern traveler.
+          © {new Date().getFullYear()} Expenses Splitter.
         </p>
       </div>
       <div className="hidden lg:block min-w-20" />
